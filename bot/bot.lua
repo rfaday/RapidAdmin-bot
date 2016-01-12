9bee0a79da9f66167bf4f35db66e5545713c3209
rfadayi <rfaday@gmail.com>
2016-01-12 14:14:00 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {142457718},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
@@ -236,7 +236,7 @@ function create_config( )
 end
 
 function on_our_id (id)
-  our_id = id
+  142457718 = id
 end
 
 function on_user_update (user, what)
